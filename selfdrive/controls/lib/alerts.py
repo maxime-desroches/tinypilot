13a94e9f157699e327a987c8c4824ba1c1446ca3
Willem Melching <willem.melching@gmail.com>
2020-03-30 18:41:56 -0700

    High speed alert (#1295)
    * add speed to high alerts
    
    * Add permanent alert
    
    * Two lines
    
    * No permanent alerts
    
    * Less bad alert
    
    * Typo

--- selfdrive/controls/lib/alerts.py
+++ selfdrive/controls/lib/alerts.py
@@ -510,6 +510,7 @@ ALERTS = [
       AlertStatus.critical, AlertSize.full,
       Priority.HIGHEST, VisualAlert.steerRequired, AudibleAlert.chimeWarningRepeat, 2.2, 3., 4.),
 
+
   # not loud cancellations (user is in control)
   Alert(
       "noTarget",
@@ -522,6 +523,13 @@ ALERTS = [
       "speedTooLow",
       "openpilot Canceled",
       "Speed too low",
+      AlertStatus.normal, AlertSize.mid,
+      Priority.HIGH, VisualAlert.none, AudibleAlert.chimeDisengage, .4, 2., 3.),
+
+  Alert(
+      "speedTooHigh",
+      "Speed Too High",
+      "Slow down to resume operation",
       AlertStatus.normal, AlertSize.mid,
       Priority.HIGH, VisualAlert.none, AudibleAlert.chimeDisengage, .4, 2., 3.),
 
@@ -693,6 +701,13 @@ ALERTS = [
       "Low Memory: Reboot Your Device",
       AlertStatus.normal, AlertSize.mid,
       Priority.LOW, VisualAlert.none, AudibleAlert.chimeDisengage, .4, 2., 3.),
+
+  Alert(
+      "speedTooHighNoEntry",
+      "Speed Too High",
+      "Slow down to engage",
+      AlertStatus.normal, AlertSize.mid,
+      Priority.LOW, VisualAlert.none, AudibleAlert.chimeError, .4, 2., 3.),
 
   # permanent alerts
   Alert(
