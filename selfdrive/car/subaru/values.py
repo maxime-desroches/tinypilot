jpevarnek/openpilot

Update driver torque scaling factors

selfdrive/car/subaru/values.py
@@ -82,9 +82,9 @@ FINGERPRINTS = {
 
 STEER_THRESHOLD = {
   CAR.IMPREZA: 80,
-  CAR.OUTBACK: 600,
-  CAR.LEGACY: 600,
-  CAR.FORESTER: 600,
+  CAR.OUTBACK: 75,
+  CAR.LEGACY: 75,
+  CAR.FORESTER: 75,
 }
 
 ECU_FINGERPRINT = {
@@ -93,7 +93,7 @@ ECU_FINGERPRINT = {
 
 DBC = {
   CAR.IMPREZA: dbc_dict('subaru_global_2017', None),
-  CAR.OUTBACK: dbc_dict('subaru_outback_2015_eyesight', None),
-  CAR.LEGACY: dbc_dict('subaru_outback_2015_eyesight', None),
-  CAR.FORESTER: dbc_dict('subaru_forester_2017', None),
+  CAR.OUTBACK: dbc_dict('subaru_outback_2015_generated', None),
+  CAR.LEGACY: dbc_dict('subaru_outback_2015_generated', None),
+  CAR.FORESTER: dbc_dict('subaru_forester_2017_generated', None),
 }

maxime-desroches/tinypilot

selfdrive/car/subaru/values.py
@@ -92,9 +92,9 @@ FINGERPRINTS = {
 STEER_THRESHOLD = {
   CAR.IMPREZA: 80,
   CAR.FORESTER: 80,
-  CAR.OUTBACK_2015: 600,
-  CAR.LEGACY_2015: 600,
-  CAR.FORESTER_2017: 600,
+  CAR.OUTBACK_2015: 75,
+  CAR.LEGACY_2015: 75,
+  CAR.FORESTER_2017: 75,
 }
 
 ECU_FINGERPRINT = {
@@ -104,7 +104,7 @@ ECU_FINGERPRINT = {
 DBC = {
   CAR.IMPREZA: dbc_dict('subaru_global_2017', None),
   CAR.FORESTER: dbc_dict('subaru_global_2017', None),
-  CAR.FORESTER_2017: dbc_dict('subaru_outback_2015_eyesight', None),
-  CAR.OUTBACK_2015: dbc_dict('subaru_outback_2015_eyesight', None),
-  CAR.LEGACY_2015: dbc_dict('subaru_outback_2015_eyesight', None),
+  CAR.FORESTER_2017: dbc_dict('subaru_forester_2017_generated', None),
+  CAR.OUTBACK_2015: dbc_dict('subaru_outback_2015_generated', None),
+  CAR.LEGACY_2015: dbc_dict('subaru_outback_2015_generated', None),
 }