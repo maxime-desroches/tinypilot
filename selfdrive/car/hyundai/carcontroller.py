warmchang/openpilot

Hyundai: Car Port for Tucson Hybrid 2022

selfdrive/car/hyundai/carcontroller.py
@@ -5,7 +5,7 @@ from common.realtime import DT_CTRL
 from opendbc.can.packer import CANPacker
 from selfdrive.car import apply_std_steer_torque_limits
 from selfdrive.car.hyundai import hda2can, hyundaican
-from selfdrive.car.hyundai.values import Buttons, CarControllerParams, HDA2_CAR, CAR
+from selfdrive.car.hyundai.values import Buttons, CarControllerParams, HDA2_CAR, CAR, CAN_FD_CAR
 
 VisualAlert = car.CarControl.HUDControl.VisualAlert
 LongCtrlState = car.CarControl.Actuators.LongControlState
@@ -54,7 +54,7 @@ class CarController:
 
     # These cars have significantly more torque than most HKG.  Limit to 70% of max.
     steer = actuators.steer
-    if self.CP.carFingerprint in (CAR.KONA, CAR.KONA_EV, CAR.KONA_HEV):
+    if self.car_fingerprint in (CAR.KONA, CAR.KONA_EV, CAR.KONA_HEV):
       steer = clip(steer, -0.7, 0.7)
     new_steer = int(round(steer * self.params.STEER_MAX))
     apply_steer = apply_std_steer_torque_limits(new_steer, self.apply_steer_last, CS.out.steeringTorque, self.params)
@@ -69,23 +69,23 @@ class CarController:
 
     can_sends = []
 
-    if self.CP.carFingerprint in HDA2_CAR:
+    if self.car_fingerprint in HDA2_CAR or CAN_FD_CAR:
       # steering control
-      can_sends.append(hda2can.create_lkas(self.packer, CC.enabled, CC.latActive, apply_steer))
+      can_sends.append(hda2can.create_lkas(self.packer, self.car_fingerprint, CC.enabled, CC.latActive, apply_steer))
 
-      if self.frame % 5 == 0:
+      if self.frame % 5 == 0 and self.car_fingerprint in HDA2_CAR:
         can_sends.append(hda2can.create_cam_0x2a4(self.packer, CS.cam_0x2a4))
 
       # cruise cancel
       if (self.frame - self.last_button_frame) * DT_CTRL > 0.25:
         if CC.cruiseControl.cancel:
           for _ in range(20):
-            can_sends.append(hda2can.create_buttons(self.packer, CS.buttons_counter+1, Buttons.CANCEL))
+            can_sends.append(hda2can.create_buttons(self.packer, self.car_fingerprint, CS.cruise_buttons_copy, CS.buttons_counter+1, Buttons.CANCEL))
           self.last_button_frame = self.frame
 
         # cruise standstill resume
         elif CC.cruiseControl.resume:
-          can_sends.append(hda2can.create_buttons(self.packer, CS.buttons_counter+1, Buttons.RES_ACCEL))
+          can_sends.append(hda2can.create_buttons(self.packer, self.car_fingerprint, CS.cruise_buttons_copy, CS.buttons_counter+1, Buttons.RES_ACCEL))
           self.last_button_frame = self.frame
     else:
 

selfdrive/car/hyundai/carstate.py
@@ -5,7 +5,7 @@ from cereal import car
 from common.conversions import Conversions as CV
 from opendbc.can.parser import CANParser
 from opendbc.can.can_define import CANDefine
-from selfdrive.car.hyundai.values import DBC, FEATURES, HDA2_CAR, EV_CAR, HYBRID_CAR, Buttons, CarControllerParams
+from selfdrive.car.hyundai.values import DBC, FEATURES, HDA2_CAR, EV_CAR, HYBRID_CAR, Buttons, CarControllerParams, CAN_FD_CAR, CAR
 from selfdrive.car.interfaces import CarStateBase
 
 PREV_BUTTON_SAMPLES = 8
@@ -19,8 +19,9 @@ class CarState(CarStateBase):
     self.cruise_buttons = deque([Buttons.NONE] * PREV_BUTTON_SAMPLES, maxlen=PREV_BUTTON_SAMPLES)
     self.main_buttons = deque([Buttons.NONE] * PREV_BUTTON_SAMPLES, maxlen=PREV_BUTTON_SAMPLES)
 
-    if CP.carFingerprint in HDA2_CAR:
-      self.shifter_values = can_define.dv["ACCELERATOR"]["GEAR"]
+    self.gear_msg_can_fd = "GEAR" if CP.carFingerprint in (CAR.TUCSON_HEV_2022, ) else "ACCELERATOR"
+    if CP.carFingerprint in HDA2_CAR or CAN_FD_CAR:
+      self.shifter_values = can_define.dv[self.gear_msg_can_fd]["GEAR"]
     elif self.CP.carFingerprint in FEATURES["use_cluster_gears"]:
       self.shifter_values = can_define.dv["CLU15"]["CF_Clu_Gear"]
     elif self.CP.carFingerprint in FEATURES["use_tcu_gears"]:
@@ -35,7 +36,7 @@ class CarState(CarStateBase):
     self.params = CarControllerParams(CP)
 
   def update(self, cp, cp_cam):
-    if self.CP.carFingerprint in HDA2_CAR:
+    if self.CP.carFingerprint in HDA2_CAR or CAN_FD_CAR:
       return self.update_hda2(cp, cp_cam)
 
     ret = car.CarState.new_message()
@@ -134,14 +135,17 @@ class CarState(CarStateBase):
   def update_hda2(self, cp, cp_cam):
     ret = car.CarState.new_message()
 
-    ret.gas = cp.vl["ACCELERATOR"]["ACCELERATOR_PEDAL"] / 255.
+    gas_scale = 1022. if self.CP.carFingerprint in (CAR.TUCSON_HEV_2022, ) else 255.
+    cruise_info_bus = cp_cam if self.CP.carFingerprint in (CAR.TUCSON_HEV_2022, ) else cp
+
+    ret.gas = cp.vl["ACCELERATOR"]["ACCELERATOR_PEDAL"] / gas_scale
     ret.gasPressed = ret.gas > 1e-3
     ret.brakePressed = cp.vl["BRAKE"]["BRAKE_PRESSED"] == 1
 
     ret.doorOpen = cp.vl["DOORS_SEATBELTS"]["DRIVER_DOOR_OPEN"] == 1
     ret.seatbeltUnlatched = cp.vl["DOORS_SEATBELTS"]["DRIVER_SEATBELT_LATCHED"] == 0
 
-    gear = cp.vl["ACCELERATOR"]["GEAR"]
+    gear = cp.vl[self.gear_msg_can_fd]["GEAR"]
     ret.gearShifter = self.parse_gear_shifter(self.shifter_values.get(gear))
 
     # TODO: figure out positions
@@ -166,22 +170,24 @@ class CarState(CarStateBase):
 
     ret.cruiseState.available = True
     ret.cruiseState.enabled = cp.vl["SCC1"]["CRUISE_ACTIVE"] == 1
-    ret.cruiseState.standstill = cp.vl["CRUISE_INFO"]["CRUISE_STANDSTILL"] == 1
+    ret.cruiseState.standstill = cruise_info_bus.vl["CRUISE_INFO"]["CRUISE_STANDSTILL"] == 1
 
     speed_factor = CV.MPH_TO_MS if cp.vl["CLUSTER_INFO"]["DISTANCE_UNIT"] == 1 else CV.KPH_TO_MS
-    ret.cruiseState.speed = cp.vl["CRUISE_INFO"]["SET_SPEED"] * speed_factor
+    ret.cruiseState.speed = cruise_info_bus.vl["CRUISE_INFO"]["SET_SPEED"] * speed_factor
 
     self.cruise_buttons.extend(cp.vl_all["CRUISE_BUTTONS"]["CRUISE_BUTTONS"])
     self.main_buttons.extend(cp.vl_all["CRUISE_BUTTONS"]["ADAPTIVE_CRUISE_MAIN_BTN"])
     self.buttons_counter = cp.vl["CRUISE_BUTTONS"]["COUNTER"]
+    self.cruise_buttons_copy = copy.copy(cp.vl["CRUISE_BUTTONS"])
 
-    self.cam_0x2a4 = copy.copy(cp_cam.vl["CAM_0x2a4"])
+    if self.CP.carFingerprint in HDA2_CAR:
+      self.cam_0x2a4 = copy.copy(cp_cam.vl["CAM_0x2a4"])
 
     return ret
 
   @staticmethod
   def get_can_parser(CP):
-    if CP.carFingerprint in HDA2_CAR:
+    if CP.carFingerprint in HDA2_CAR or CAN_FD_CAR:
       return CarState.get_can_parser_hda2(CP)
 
     signals = [
@@ -316,10 +322,13 @@ class CarState(CarStateBase):
 
   @staticmethod
   def get_cam_can_parser(CP):
-    if CP.carFingerprint in HDA2_CAR:
-      signals = [(f"BYTE{i}", "CAM_0x2a4") for i in range(3, 24)]
-      checks = [("CAM_0x2a4", 20)]
-      return CANParser(DBC[CP.carFingerprint]["pt"], signals, checks, 6)
+    if CP.carFingerprint in HDA2_CAR or CAN_FD_CAR:
+      if CP.carFingerprint in (CAR.TUCSON_HEV_2022, ):
+        return CarState.get_cam_can_parser_can_fd(CP)
+      elif CP.carFingerprint in HDA2_CAR:
+        signals = [(f"BYTE{i}", "CAM_0x2a4") for i in range(3, 24)]
+        checks = [("CAM_0x2a4", 20)]
+        return CANParser(DBC[CP.carFingerprint]["pt"], signals, checks, 6)
 
     signals = [
       # signal_name, signal_address
@@ -354,7 +363,6 @@ class CarState(CarStateBase):
       ("WHEEL_SPEED_4", "WHEEL_SPEEDS"),
 
       ("ACCELERATOR_PEDAL", "ACCELERATOR"),
-      ("GEAR", "ACCELERATOR"),
       ("BRAKE_PRESSED", "BRAKE"),
 
       ("STEERING_RATE", "STEERING_SENSORS"),
@@ -363,8 +371,6 @@ class CarState(CarStateBase):
       ("STEERING_OUT_TORQUE", "MDPS"),
 
       ("CRUISE_ACTIVE", "SCC1"),
-      ("SET_SPEED", "CRUISE_INFO"),
-      ("CRUISE_STANDSTILL", "CRUISE_INFO"),
       ("COUNTER", "CRUISE_BUTTONS"),
       ("CRUISE_BUTTONS", "CRUISE_BUTTONS"),
       ("ADAPTIVE_CRUISE_MAIN_BTN", "CRUISE_BUTTONS"),
@@ -385,11 +391,34 @@ class CarState(CarStateBase):
       ("STEERING_SENSORS", 100),
       ("MDPS", 100),
       ("SCC1", 50),
-      ("CRUISE_INFO", 50),
       ("CRUISE_BUTTONS", 50),
       ("CLUSTER_INFO", 4),
       ("BLINKERS", 4),
       ("DOORS_SEATBELTS", 4),
     ]
 
-    return CANParser(DBC[CP.carFingerprint]["pt"], signals, checks, 5)
+    if CP.carFingerprint in (CAR.TUCSON_HEV_2022, ):
+      signals.append(("GEAR", "GEAR"))
+      checks.append(("GEAR", 100))
+    else:
+      signals += [
+        ("SET_SPEED", "CRUISE_INFO"),
+        ("CRUISE_STANDSTILL", "CRUISE_INFO"),
+      ]
+      checks.append(("CRUISE_INFO", 50))
+
+    bus = 5 if CP.carFingerprint in HDA2_CAR else 4
+
+    return CANParser(DBC[CP.carFingerprint]["pt"], signals, checks, bus)
+
+  @staticmethod
+  def get_cam_can_parser_can_fd(CP):
+    signals = [
+      ("SET_SPEED", "CRUISE_INFO"),
+      ("CRUISE_STANDSTILL", "CRUISE_INFO"),
+    ]
+    checks = [
+      ("CRUISE_INFO", 50),
+    ]
+
+    return CANParser(DBC[CP.carFingerprint]["pt"], signals, checks, 6)

selfdrive/car/hyundai/interface.py
@@ -163,6 +163,14 @@ class CarInterface(CarInterfaceBase):
       tire_stiffness_factor = 0.385
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.25], [0.05]]
+    elif candidate == CAR.TUCSON_HEV_2022:
+      ret.mass = 1680. + STD_CARGO_KG  # average of the 3 trims https://www.hyundaiusa.com/us/en/vehicles/tucson-hybrid/compare-specs
+      ret.wheelbase = 2.756
+      ret.steerRatio = 16.
+      ret.safetyConfigs = [get_safety_config(car.CarParams.SafetyModel.noOutput),
+                           get_safety_config(car.CarParams.SafetyModel.hyundaiHDA2)]
+      tire_stiffness_factor = 0.385
+      CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
 
     # Kia
     elif candidate == CAR.KIA_SORENTO:
@@ -304,6 +312,9 @@ class CarInterface(CarInterfaceBase):
 
     if ret.openpilotLongitudinalControl:
       ret.safetyConfigs[0].safetyParam |= Panda.FLAG_HYUNDAI_LONG
+
+    if candidate == CAR.TUCSON_HEV_2022:
+      ret.safetyConfigs[1].safetyParam = Panda.FLAG_HYUNDAI_TUCSON_HEV_2022
 
     return ret
 

maxime-desroches/tinypilot

selfdrive/car/hyundai/carcontroller.py
@@ -54,7 +54,7 @@ class CarController:
 
     # These cars have significantly more torque than most HKG.  Limit to 70% of max.
     steer = actuators.steer
-    if self.CP.carFingerprint in (CAR.KONA, CAR.KONA_EV, CAR.KONA_HEV, CAR.KONA_EV_2022):
+    if self.car_fingerprint in (CAR.KONA, CAR.KONA_EV, CAR.KONA_HEV, CAR.KONA_EV_2022):
       steer = clip(steer, -0.7, 0.7)
     new_steer = int(round(steer * self.params.STEER_MAX))
     apply_steer = apply_std_steer_torque_limits(new_steer, self.apply_steer_last, CS.out.steeringTorque, self.params)
@@ -69,23 +69,23 @@ class CarController:
 
     can_sends = []
 
-    if self.CP.carFingerprint in CANFD_CAR:
+    if self.car_fingerprint in CANFD_CAR:
       # steering control
-      can_sends.append(hyundaicanfd.create_lkas(self.packer, CC.enabled, CC.latActive, apply_steer))
+      can_sends.append(hyundaicanfd.create_lkas(self.packer, self.car_fingerprint, CC.enabled, CC.latActive, apply_steer))
 
-      if self.frame % 5 == 0:
+      if self.frame % 5 == 0 and self.car_fingerprint not in (CAR.TUCSON_HYBRID_4TH_GEN, ):
         can_sends.append(hyundaicanfd.create_cam_0x2a4(self.packer, CS.cam_0x2a4))
 
       # cruise cancel
       if (self.frame - self.last_button_frame) * DT_CTRL > 0.25:
         if CC.cruiseControl.cancel:
           for _ in range(20):
-            can_sends.append(hyundaicanfd.create_buttons(self.packer, CS.buttons_counter+1, Buttons.CANCEL))
+            can_sends.append(hyundaicanfd.create_buttons(self.packer, self.car_fingerprint, CS.cruise_buttons_copy, CS.buttons_counter+1, Buttons.CANCEL))
           self.last_button_frame = self.frame
 
         # cruise standstill resume
         elif CC.cruiseControl.resume:
-          can_sends.append(hyundaicanfd.create_buttons(self.packer, CS.buttons_counter+1, Buttons.RES_ACCEL))
+          can_sends.append(hyundaicanfd.create_buttons(self.packer, self.car_fingerprint, CS.cruise_buttons_copy, CS.buttons_counter+1, Buttons.RES_ACCEL))
           self.last_button_frame = self.frame
     else:
 

selfdrive/car/hyundai/carstate.py
@@ -5,7 +5,7 @@ from cereal import car
 from common.conversions import Conversions as CV
 from opendbc.can.parser import CANParser
 from opendbc.can.can_define import CANDefine
-from selfdrive.car.hyundai.values import DBC, FEATURES, CAMERA_SCC_CAR, CANFD_CAR, EV_CAR, HYBRID_CAR, Buttons, CarControllerParams
+from selfdrive.car.hyundai.values import DBC, FEATURES, CAMERA_SCC_CAR, CANFD_CAR, EV_CAR, HYBRID_CAR, Buttons, CarControllerParams, CAR
 from selfdrive.car.interfaces import CarStateBase
 
 PREV_BUTTON_SAMPLES = 8
@@ -20,7 +20,10 @@ class CarState(CarStateBase):
     self.main_buttons = deque([Buttons.NONE] * PREV_BUTTON_SAMPLES, maxlen=PREV_BUTTON_SAMPLES)
 
     if CP.carFingerprint in CANFD_CAR:
-      self.shifter_values = can_define.dv["ACCELERATOR"]["GEAR"]
+      if CP.carFingerprint in (CAR.TUCSON_HYBRID_4TH_GEN, ):
+        self.shifter_values = can_define.dv["GEAR"]["GEAR"]
+      else:
+        self.shifter_values = can_define.dv["ACCELERATOR"]["GEAR"]
     elif self.CP.carFingerprint in FEATURES["use_cluster_gears"]:
       self.shifter_values = can_define.dv["CLU15"]["CF_Clu_Gear"]
     elif self.CP.carFingerprint in FEATURES["use_tcu_gears"]:
@@ -136,14 +139,18 @@ class CarState(CarStateBase):
   def update_canfd(self, cp, cp_cam):
     ret = car.CarState.new_message()
 
-    ret.gas = cp.vl["ACCELERATOR"]["ACCELERATOR_PEDAL"] / 255.
+    gas_scale = 1022. if self.CP.carFingerprint in (CAR.TUCSON_HYBRID_4TH_GEN, ) else 255.
+    cruise_info_bus = cp_cam if self.CP.carFingerprint in (CAR.TUCSON_HYBRID_4TH_GEN, ) else cp
+    gear_msg = "GEAR" if self.CP.carFingerprint in (CAR.TUCSON_HYBRID_4TH_GEN, ) else "ACCELERATOR"
+
+    ret.gas = cp.vl["ACCELERATOR"]["ACCELERATOR_PEDAL"] / gas_scale
     ret.gasPressed = ret.gas > 1e-3
     ret.brakePressed = cp.vl["BRAKE"]["BRAKE_PRESSED"] == 1
 
     ret.doorOpen = cp.vl["DOORS_SEATBELTS"]["DRIVER_DOOR_OPEN"] == 1
     ret.seatbeltUnlatched = cp.vl["DOORS_SEATBELTS"]["DRIVER_SEATBELT_LATCHED"] == 0
 
-    gear = cp.vl["ACCELERATOR"]["GEAR"]
+    gear = cp.vl[gear_msg]["GEAR"]
     ret.gearShifter = self.parse_gear_shifter(self.shifter_values.get(gear))
 
     # TODO: figure out positions
@@ -168,16 +175,18 @@ class CarState(CarStateBase):
 
     ret.cruiseState.available = True
     ret.cruiseState.enabled = cp.vl["SCC1"]["CRUISE_ACTIVE"] == 1
-    ret.cruiseState.standstill = cp.vl["CRUISE_INFO"]["CRUISE_STANDSTILL"] == 1
+    ret.cruiseState.standstill = cruise_info_bus.vl["CRUISE_INFO"]["CRUISE_STANDSTILL"] == 1
 
     speed_factor = CV.MPH_TO_MS if cp.vl["CLUSTER_INFO"]["DISTANCE_UNIT"] == 1 else CV.KPH_TO_MS
-    ret.cruiseState.speed = cp.vl["CRUISE_INFO"]["SET_SPEED"] * speed_factor
+    ret.cruiseState.speed = cruise_info_bus.vl["CRUISE_INFO"]["SET_SPEED"] * speed_factor
 
     self.cruise_buttons.extend(cp.vl_all["CRUISE_BUTTONS"]["CRUISE_BUTTONS"])
     self.main_buttons.extend(cp.vl_all["CRUISE_BUTTONS"]["ADAPTIVE_CRUISE_MAIN_BTN"])
     self.buttons_counter = cp.vl["CRUISE_BUTTONS"]["COUNTER"]
+    self.cruise_buttons_copy = copy.copy(cp.vl["CRUISE_BUTTONS"])
 
-    self.cam_0x2a4 = copy.copy(cp_cam.vl["CAM_0x2a4"])
+    if self.CP.carFingerprint in (CANFD_CAR and not (CAR.TUCSON_HYBRID_4TH_GEN, )):
+      self.cam_0x2a4 = copy.copy(cp_cam.vl["CAM_0x2a4"])
 
     return ret
 
@@ -319,9 +328,12 @@ class CarState(CarStateBase):
   @staticmethod
   def get_cam_can_parser(CP):
     if CP.carFingerprint in CANFD_CAR:
-      signals = [(f"BYTE{i}", "CAM_0x2a4") for i in range(3, 24)]
-      checks = [("CAM_0x2a4", 20)]
-      return CANParser(DBC[CP.carFingerprint]["pt"], signals, checks, 6)
+      if CP.carFingerprint in (CAR.TUCSON_HYBRID_4TH_GEN, ):
+        return CarState.get_cam_can_parser_can_fd(CP)
+      else:
+        signals = [(f"BYTE{i}", "CAM_0x2a4") for i in range(3, 24)]
+        checks = [("CAM_0x2a4", 20)]
+        return CANParser(DBC[CP.carFingerprint]["pt"], signals, checks, 6)
 
     signals = [
       # signal_name, signal_address
@@ -381,7 +393,6 @@ class CarState(CarStateBase):
       ("WHEEL_SPEED_4", "WHEEL_SPEEDS"),
 
       ("ACCELERATOR_PEDAL", "ACCELERATOR"),
-      ("GEAR", "ACCELERATOR"),
       ("BRAKE_PRESSED", "BRAKE"),
 
       ("STEERING_RATE", "STEERING_SENSORS"),
@@ -390,8 +401,6 @@ class CarState(CarStateBase):
       ("STEERING_OUT_TORQUE", "MDPS"),
 
       ("CRUISE_ACTIVE", "SCC1"),
-      ("SET_SPEED", "CRUISE_INFO"),
-      ("CRUISE_STANDSTILL", "CRUISE_INFO"),
       ("COUNTER", "CRUISE_BUTTONS"),
       ("CRUISE_BUTTONS", "CRUISE_BUTTONS"),
       ("ADAPTIVE_CRUISE_MAIN_BTN", "CRUISE_BUTTONS"),
@@ -412,11 +421,34 @@ class CarState(CarStateBase):
       ("STEERING_SENSORS", 100),
       ("MDPS", 100),
       ("SCC1", 50),
-      ("CRUISE_INFO", 50),
       ("CRUISE_BUTTONS", 50),
       ("CLUSTER_INFO", 4),
       ("BLINKERS", 4),
       ("DOORS_SEATBELTS", 4),
     ]
 
-    return CANParser(DBC[CP.carFingerprint]["pt"], signals, checks, 5)
+    if CP.carFingerprint in (CAR.TUCSON_HYBRID_4TH_GEN, ):
+      signals.append(("GEAR", "GEAR"))
+      checks.append(("GEAR", 100))
+    else:
+      signals += [
+        ("SET_SPEED", "CRUISE_INFO"),
+        ("CRUISE_STANDSTILL", "CRUISE_INFO"),
+      ]
+      checks.append(("CRUISE_INFO", 50))
+
+    bus = 4 if CP.carFingerprint in (CAR.TUCSON_HYBRID_4TH_GEN, ) else 5
+
+    return CANParser(DBC[CP.carFingerprint]["pt"], signals, checks, bus)
+
+  @staticmethod
+  def get_cam_can_parser_can_fd(CP):
+    signals = [
+      ("SET_SPEED", "CRUISE_INFO"),
+      ("CRUISE_STANDSTILL", "CRUISE_INFO"),
+    ]
+    checks = [
+      ("CRUISE_INFO", 50),
+    ]
+
+    return CANParser(DBC[CP.carFingerprint]["pt"], signals, checks, 6)

selfdrive/car/hyundai/interface.py
@@ -163,6 +163,14 @@ class CarInterface(CarInterfaceBase):
       tire_stiffness_factor = 0.385
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.25], [0.05]]
+    elif candidate == CAR.TUCSON_HYBRID_4TH_GEN:
+      ret.mass = 1680. + STD_CARGO_KG  # average of the 3 trims https://www.hyundaiusa.com/us/en/vehicles/tucson-hybrid/compare-specs
+      ret.wheelbase = 2.756
+      ret.steerRatio = 16.
+      ret.safetyConfigs = [get_safety_config(car.CarParams.SafetyModel.noOutput),
+                           get_safety_config(car.CarParams.SafetyModel.hyundaiHDA2)]
+      tire_stiffness_factor = 0.385
+      CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
 
     # Kia
     elif candidate == CAR.KIA_SORENTO:
@@ -287,6 +295,8 @@ class CarInterface(CarInterfaceBase):
     if candidate in CANFD_CAR:
       ret.safetyConfigs = [get_safety_config(car.CarParams.SafetyModel.noOutput),
                            get_safety_config(car.CarParams.SafetyModel.hyundaiCanfd)]
+      if candidate == CAR.TUCSON_HYBRID_4TH_GEN:
+        ret.safetyConfigs[1].safetyParam = Panda.FLAG_HYUNDAI_TUCSON_HEV_2022
     else:
       if candidate in LEGACY_SAFETY_MODE_CAR:
         # these cars require a special panda safety mode due to missing counters and checksums in the messages