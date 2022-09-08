ogiechogie/openpilot

Ford: add shiftByWire ECU fw

selfdrive/car/fw_versions.py
@@ -240,7 +240,7 @@ REQUESTS: List[Request] = [
     [TESTER_PRESENT_REQUEST, FORD_VERSION_REQUEST],
     [TESTER_PRESENT_RESPONSE, FORD_VERSION_RESPONSE],
     bus=0,
-    whitelist_ecus=[Ecu.eps, Ecu.esp, Ecu.fwdRadar, Ecu.fwdCamera],
+    whitelist_ecus=[Ecu.eps, Ecu.esp, Ecu.fwdRadar, Ecu.fwdCamera, Ecu.shiftByWire],
   ),
 ]
 

maxime-desroches/tinypilot

selfdrive/car/fw_versions.py
@@ -240,7 +240,7 @@ REQUESTS: List[Request] = [
     [TESTER_PRESENT_REQUEST, FORD_VERSION_REQUEST],
     [TESTER_PRESENT_RESPONSE, FORD_VERSION_RESPONSE],
     bus=0,
-    whitelist_ecus=[Ecu.eps, Ecu.abs, Ecu.fwdRadar, Ecu.fwdCamera],
+    whitelist_ecus=[Ecu.eps, Ecu.abs, Ecu.fwdRadar, Ecu.fwdCamera, Ecu.shiftByWire],
   ),
 ]
 